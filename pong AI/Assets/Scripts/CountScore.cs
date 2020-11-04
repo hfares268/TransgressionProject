b9431a145b7ec0034dcdc4952195929bdd4d9409
Hassan Fares <hfares268@gmail.com>
2020-11-04 02:47:08 -0500

    pong scoreboard

---
+++ pong AI/Assets/Scripts/CountScore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountScore : MonoBehaviour
+{
+    public Text Scoreboard;
+    public GameObject ball;
+    public Text scoreText;
+
+    private int player1score = 0;
+    private int player2score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GameObject.Find("Ball");
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter()
+    {
+        player1score++;
+        ball.transform.position = new Vector2(0, transform.position.y);
+        scoreText.text = "Player 1:" + player1score;
+    }
+    /*
+        if (ball.transform.position.x <= -17)
+        {
+            player2score++;
+        }
+
+        Scoreboard.text = player1score.ToString() + " - " + player2score.ToString();
+
+        print(player1score + " , " + player2score);
+        */
+    }
--- pong AI/Assets/Scripts/Score.cs
+++ pong AI/Assets/Scripts/Score.cs
@@ -5,14 +5,17 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    public Transform ball;
+    
+    public Transform player;
     public Text scoreText;
+    float num = 5;
     // Start is called before the first frame update
    
 
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = ball.position.ToString("0");
+        scoreText.text = player.position.ToString();
+        num = num++;
     }
 }
