f5fa55e94ee8ebe6070db9744010d63c4d85c64f
harrison humphrey <harrison.humphrey@mail.citytech.cuny.edu>
2020-11-04 11:45:26 -0500

    Attempt at fixing score

---
+++ pong AI/Assets/EnemyScoreScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScoreScript : MonoBehaviour
+{
+    private int Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        Addscore();
+    }
+
+    void Addscore()
+    {
+        Score++;
+        // EnemyScoreText.text = Score; 
+    }
+}
---
+++ pong AI/Assets/PlayerScore.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour
+{
+    private int Score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        Addscore();
+    }
+
+    void Addscore()
+    {
+        Score++;
+        //PlayerScoreText.text = Score;
+    }
+}
+
